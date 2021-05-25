c4f8f941ebe0b1232982013e7c6349bed816675a
JeffinGBenny <69665999+JeffinGBenny@users.noreply.github.com>
2021-05-26 00:52:14 +0530

    Update admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-<<<<<<< HEAD
+
 from .models import User_Details,EventDetails
 
 admin.site.register(User_Details),
 admin.site.register(EventDetails)
-=======
 
->>>>>>> 3cd566f... success
 # Register your models here.
