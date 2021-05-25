d143c42b3161d82583cb7685dcc2b0f86f5e098b
JeffinGBenny <69665999+JeffinGBenny@users.noreply.github.com>
2021-05-26 01:28:13 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -2,14 +2,11 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.models import User,auth
-
 from .models import EventDetails,User_Details
-
 def login(request):
     if request.method=='POST':
         username=request.POST['username']
         password=request.POST['password']
-
         if User_Details.objects.filter(username=username).exists():
             if User_Details.objects.filter(username=username).exists():
                 details=EventDetails.objects.all()
@@ -22,23 +19,12 @@ def login(request):
 
 def UserProfile(request):
      return render(request,"UserProfile.html")
-     user=auth.authenticate(username=username,password=password)
-        
-        if user is not None:
-            auth.login(request,user)
-            return render(request,'bfhpage2org.html')
-        else:
-            messages.info(request,'invalid credentials')
-
-     return render(request,"login.html")
-
 
 def  createEvent(request):
     if request.method=='POST':
         eventtitle=request.POST['eventtitle']
         Date=request.POST['Date']
         Time=request.POST['Time']
-
         Location=request.POST['Location']
         max_no_participants=request.POST['max_no_participants']
         Description=request.POST['Description']
@@ -67,41 +53,10 @@ def  signup(request):
             messages.info(request,'user created')
             return render(request,"login.html")
 
-        max_no_participants=request.POST['max_no_participants']
-        Description=request.POST['Description']
-        eventbanner=request.POST['eventbanner']
-        if User.objects.filter(username=username).exists():
-            messages.info(request,'username taken')
-            return redirect('/')
-        else:
-            user=User.objects.create_user(username=username,password=password,semester=semester,mobile=mobile,email=email)
-            user.save()
-            messages.info('user created'
-            )
-
-    else:
-        return redirect("/")
-def  signup(request):
-    if request.method=='POST':
-        username=request.POST['username']
-        semester=request.POST['semester']
-        mobile=request.POST['mobile']
-        email=request.POST['email']
-        password=request.POST['password']
-        if User.objects.filter(username=username).exists():
-            messages.info(request,'username taken')
-            return redirect('/')
-        else:
-            user=User.objects.create_user(username=username,password=password,semester=semester,mobile=mobile,email=email)
-            user.save()
-            messages.info('user created')
-
-
     else:
         return render(request,'signup.html')
 def logout(request):
     auth.logout(request)
-
     return render(request,"login.html")
 
 def  showEvent(request):
@@ -118,25 +73,3 @@ def eventRegister(request):
         messages.info(request,'Successfully Registered')
         registeredevent=request.POST['registeredevent']
     
-
-    return redirect('/')
-    def  showEvent(request):
-        if request.method=='GET':
-            eventtitle=request.GET['eventtitle']
-            Date=request.GET['Date']
-            Time=request.GET['Time']
-            max_no_participants=request.GET['max_no_participants']
-            Description=request.GET['Description']
-            eventbanner=request.GET['eventbanner']
-            if User.objects.filter(username=username).exists():
-                messages.info(request,'username taken')
-                return redirect('/')
-            else:
-                user=User.objects.create_user(username=username,password=password,semester=semester,mobile=mobile,email=email)
-                user.save()
-                messages.info('user created'
-                )
-
-        else:
-            return redirect("/")
-
